035134bccfdcab3090832cb7f55d234cf2c39b15
LXJ <139714671480@163.com>
2018-03-05 14:10:59 +0800

    图片资源

--- DYZB/DYZB/ViewController.swift
+++ DYZB/DYZB/ViewController.swift
@@ -13,13 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let _ = UIAlertController(title: "q", message: "t", preferredStyle: .alert)
+
+        let btn = UIButton()
+        btn.addTarget(self, action: #selector(qwe), for: .touchUpInside)
+    }
+    @objc func qwe() -> () {
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
